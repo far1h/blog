9d45643926467b1d38ca34923c9ee413c4e5ac6a
far1h <farihmuhammad45@gmail.com>
2024-11-08 15:55:30 +0700

    Make pagination darkmode

--- components/blog/pagination.tsx
+++ components/blog/pagination.tsx
@@ -1,60 +1,92 @@
+import { useTheme } from '../contexts/ThemeContext';
+
 type Props = {
-    currPage: number
-    maxPage: number
-  }
-  
-  function getPaginationArr(currPage: number, maxPage: number, numItems: number = 5) {
-    const pageList = [currPage]
-    while (pageList.length < numItems) {
-      const leftDelta = Math.abs(pageList[0] - currPage);
-      const rightDelta = Math.abs(pageList[pageList.length - 1] - currPage);
-      if ((leftDelta <= rightDelta || pageList[pageList.length - 1] == maxPage) && pageList[0] > 1) {
-        pageList.unshift(pageList[0] - 1);
-      } else if ((leftDelta > rightDelta || pageList[0] == 1) && pageList[pageList.length - 1] < maxPage) {
-        pageList.push(pageList[pageList.length - 1] + 1);
-      } else {
-        break;
-      }
+  currPage: number;
+  maxPage: number;
+};
+
+function getPaginationArr(currPage: number, maxPage: number, numItems: number = 5) {
+  const pageList = [currPage];
+  while (pageList.length < numItems) {
+    const leftDelta = Math.abs(pageList[0] - currPage);
+    const rightDelta = Math.abs(pageList[pageList.length - 1] - currPage);
+    if ((leftDelta <= rightDelta || pageList[pageList.length - 1] === maxPage) && pageList[0] > 1) {
+      pageList.unshift(pageList[0] - 1);
+    } else if ((leftDelta > rightDelta || pageList[0] === 1) && pageList[pageList.length - 1] < maxPage) {
+      pageList.push(pageList[pageList.length - 1] + 1);
+    } else {
+      break;
     }
-    return pageList;
   }
-  
-  function Pagination({ currPage, maxPage }: Props) {
-    const paginationArr = getPaginationArr(currPage, maxPage);
-    return (
-      <div className="flex justify-center mb-10">
-          <ul className="inline-flex -space-x-px">
-            <li key="previous">
-              {(currPage === 1) ? (
-                <a className="bg-white border border-gray-300 text-gray-500 ml-0 rounded-l-lg leading-tight py-2 px-3 cursor-default">Previous</a>
-              ): (
-                <a href={`/posts/${currPage - 1}`}
-                  className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 ml-0 rounded-l-lg leading-tight py-2 px-3 ">Previous</a>
-              )} 
-            </li>
-            {paginationArr.map((i) => (i === currPage) ? (
-              <li key={i}>
-                <a href="" aria-current="page"
-                  className="bg-blue-50 border border-gray-300 text-blue-600 hover:bg-blue-100 hover:text-blue-700 py-2 px-3">{i}</a>
-              </li>
+  return pageList;
+}
+
+function Pagination({ currPage, maxPage }: Props) {
+  const paginationArr = getPaginationArr(currPage, maxPage);
+  const { theme } = useTheme();
+
+  const baseClass = `border px-3 py-2 leading-tight`;
+  const lightModeClass = `bg-white  text-gray-500 hover:bg-gray-100 hover:text-gray-700`;
+  const darkModeClass = `bg-gray-800 border-gray-600 text-gray-400 hover:bg-gray-700 hover:text-gray-300`;
+
+  const currentClass = `border-blue-500 bg-blue-100 text-blue-700`;
+
+  return (
+    <div className="flex justify-center mb-10">
+      <ul className="inline-flex -space-x-px">
+        <li key="previous">
+          {currPage === 1 ? (
+            <span
+              className={`${baseClass} ${theme === 'dark' ? darkModeClass : lightModeClass} rounded-l-lg cursor-default`}
+            >
+              Previous
+            </span>
+          ) : (
+            <a
+              href={`/posts/${currPage - 1}`}
+              className={`${baseClass} ${theme === 'dark' ? darkModeClass : lightModeClass} rounded-l-lg`}
+            >
+              Previous
+            </a>
+          )}
+        </li>
+        {paginationArr.map((i) => (
+          <li key={i}>
+            {i === currPage ? (
+              <span
+                className={`${baseClass} ${currentClass}`}
+              >
+                {i}
+              </span>
             ) : (
-              <li key={i}>
-                <a href={`/posts/${i}`}
-                  className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 ">{i}</a>
-              </li>
-            ))}
-            <li key="next">
-              {(currPage === maxPage) ? (
-                <a className={`bg-white border border-gray-300 text-gray-500 rounded-r-lg leading-tight py-2 px-3 cursor-default`}>Next</a>
-              ): (
-                <a href={`/posts/${currPage + 1}`}
-                  className={`bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 rounded-r-lg leading-tight py-2 px-3`}>Next</a>
-              )} 
-            </li>
-          </ul>
-      </div>
-    )
-  }
-  
-  export default Pagination;
-  
+              <a
+                href={`/posts/${i}`}
+                className={`${baseClass} ${theme === 'dark' ? darkModeClass : lightModeClass}`}
+              >
+                {i}
+              </a>
+            )}
+          </li>
+        ))}
+        <li key="next">
+          {currPage === maxPage ? (
+            <span
+              className={`${baseClass} ${theme === 'dark' ? darkModeClass : lightModeClass} rounded-r-lg cursor-default`}
+            >
+              Next
+            </span>
+          ) : (
+            <a
+              href={`/posts/${currPage + 1}`}
+              className={`${baseClass} ${theme === 'dark' ? darkModeClass : lightModeClass} rounded-r-lg`}
+            >
+              Next
+            </a>
+          )}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Pagination;
