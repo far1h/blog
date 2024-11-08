92c6fe3149d951eb5c558a03035c97fcc9a076e5
far1h <farihmuhammad45@gmail.com>
2024-11-08 15:55:53 +0700

    try fix pid not working

--- pages/posts/[pid].tsx
+++ pages/posts/[pid].tsx
@@ -1,38 +1,32 @@
-import { getAllPosts } from '../../lib/api'
-import Post from '../../interfaces/post'
-import PostList from '../../components/blog/post-list'
-import Pagination from '../../components/blog/pagination'
-import PostType from '../../interfaces/post'
-import Layout from '../../components/misc/layout'
+import { getAllPosts } from '../../lib/api';
+import Post from '../../interfaces/post';
+import PostList from '../../components/blog/post-list';
+import Pagination from '../../components/blog/pagination';
+import Layout from '../../components/misc/layout';
 
 type Props = {
-  posts: Post[]
-  pid: number
-  maxPid: number
-}
+  posts: Post[];
+  pid: number;
+  maxPid: number;
+};
 
 export default function Index({ posts, pid, maxPid }: Props) {
   return (
     <Layout>
-      <PostList posts={posts || []}/>
-      <Pagination currPage={pid} maxPage={maxPid}/>
+      <section id="posts">
+        <PostList posts={posts || []} />
+        <Pagination currPage={pid} maxPage={maxPid} />
+      </section>
     </Layout>
-  )
-}
-
-type Params = {
-  params: {
-    pid: string
-    posts: PostType[]
-  }
+  );
 }
 
 const pageSize = 6;
 const filterPosts = (posts: any[]) => {
-  return posts
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
-}
-export const getStaticProps = async ({ params }: Params) => {
+  return posts.sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+};
+
+export const getStaticProps = async ({ params }: { params: { pid: string } }) => {
   let posts = await getAllPosts([
     'title',
     'date',
@@ -42,34 +36,33 @@ export const getStaticProps = async ({ params }: Params) => {
     'excerpt',
   ]);
   posts = filterPosts(posts);
-  const pid = parseInt(params.pid)
-  const maxPid = Math.round(posts.length / pageSize)
+
+  const pid = parseInt(params.pid, 10);
+  const maxPid = Math.ceil(posts.length / pageSize);
   const start = (pid - 1) * pageSize;
-  posts = posts.slice(start, start + pageSize)
+  posts = posts.slice(start, start + pageSize);
 
   return {
     props: { posts, pid, maxPid },
-  }
-}
+  };
+};
 
-export async function getStaticPaths() {
-  let posts = await getAllPosts(['slug'])
-  // filter posts shown here
+export const getStaticPaths = async () => {
+  let posts = await getAllPosts(['slug']);
   posts = filterPosts(posts);
 
-  const paths = []
+  const paths = [];
   let pid = 1;
   for (let i = 0; i < posts.length; i += pageSize) {
-    if (pid === 1) continue; // Skip pid=1, as it's handled by `index.tsx`
     paths.push({
       params: {
-        pid: pid.toString(),
-      }
+        pid: (pid++).toString(),
+      },
     });
-    pid += 1;
   }
+
   return {
-    paths: paths,
+    paths,
     fallback: false,
-  }
-}
+  };
+};
