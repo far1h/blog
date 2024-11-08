936c3303224ad2c85492bdc21e028fe3bf66559d
far1h <farihmuhammad45@gmail.com>
2024-11-08 17:57:28 +0700

    Add darkmode finally to post body and also internal links

--- components/post/post-body.tsx
+++ components/post/post-body.tsx
@@ -1,16 +1,21 @@
-import markdownStyles from './markdown-styles.module.css'
+import markdownStyles from './markdown-styles.module.css';
+import { useTheme } from '../contexts/ThemeContext';
 
 type Props = {
-  content: string
-}
+  content: string;
+};
 
 const PostBody = ({ content }: Props) => {
+  const { theme } = useTheme(); // Get current theme
+
   return (
     <div
-      className={markdownStyles['markdown-body']}
+      className={`${markdownStyles['markdown-body']} ${
+        theme === 'dark' ? markdownStyles['dark'] : markdownStyles['light']
+      }`}
       dangerouslySetInnerHTML={{ __html: content }}
     />
-  )
-}
+  );
+};
 
-export default PostBody
+export default PostBody;
